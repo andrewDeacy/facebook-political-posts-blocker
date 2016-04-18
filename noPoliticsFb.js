c53c5a94ee89a9b33f48823cf58aa76ea85fe8a9
Mike Boyle <mikesboyle@gmail.com>
2016-04-17 21:36:17 -0400

    better way to find posts

--- noPoliticsFb.js
+++ noPoliticsFb.js
@@ -1,22 +1,24 @@
 function hidePoliticalPosts() {
-    console.log('checking...');
-    var forbidden = ['bernie', 'hillary', 'sanders', 'clinton', 'trump', 'drumpf', 'election', '#feelthebern'];
+    var forbidden = ['bernie', 'hillary', 'sanders', 'clinton', 'trump', 'drumpf', 'election', '#feelthebern', 'cruz', 'rubio'];
     var posts = document.getElementsByClassName('userContentWrapper');
-    for (var i = 0; i < posts.length; i ++) {
-       var post = posts[i];
-       var postText = post.textContent.toLowerCase();
-       if (forbidden.some(function(v) { return postText.indexOf(v) >= 0; })) {
-        console.log('found one!');
-        console.log(postText);
-        post.style.display = 'none';
-       }
+    if (posts.length < 1) {
+        // keep looking until .userContentWrapper divs appear
+        window.requestAnimationFrame(hidePoliticalPosts);
+    } else {
+        for (var i = 0; i < posts.length; i ++) {
+            var post = posts[i];
+            var postText = post.textContent.toLowerCase();
+            if (forbidden.some(function(v) { return postText.indexOf(v) >= 0; })) {
+                post.style.display = 'none';
+            }
+        }
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  hidePoliticalPosts();
-  window.onscroll = function() {
-      console.log('scrolling...');
-      hidePoliticalPosts();
-  }
-});
+window.onscroll = function() {
+    hidePoliticalPosts();
+};
+
+window.onload = function() {
+    hidePoliticalPosts();
+};
